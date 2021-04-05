1f8fa2c9326321ff4372935facc6056305d3165d
nimblefrog <nimblefrog0731@gmail.com>
2021-04-05 22:21:03 +0800

    上傳會員機制的產品管理系統(未完成)

---
+++ ch06/slnProductSys/prjProductSys/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjProductSys.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        //加上Authorize後，必須登入後才能使用這個動作方法
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ch06/slnProductSys/prjProductSys/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using prjProductSys.Models;
+
+namespace prjProductSys.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string account, string pwd)
+        {
+            var db = new dbProductEntities();
+            var member = db.會員.FirstOrDefault(x => x.帳號 == account && x.密碼 == pwd);
+            if(member != null)
+            {
+                FormsAuthentication.RedirectFromLoginPage(member.帳號, true);
+                return RedirectToAction("Index", "Category");
+            }
+
+            ViewBag.IsLogin = true;
+            return View();
+        }
+    }
+}
